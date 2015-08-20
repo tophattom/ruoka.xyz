dc0ec15e44bce811c2ecce840f1b41c9252245a8
Jaakko Rinta-Filppula <jaakko.rf@gmail.com>
2015-08-20 14:58:44 +0300

    add buttons for changing day

--- js/main.js
+++ js/main.js
@@ -1,23 +1,48 @@
 (function() {
     'use strict';
-
+    
+    var selectedDate = moment(new Date());
+        
     var contentElem = document.getElementById('main-content'),
     
         source = document.getElementById('main-template').innerHTML,
         template = Handlebars.compile(source),
         
-        apiUrl = 'http://api.ruoka.xyz/',
-        dateString = (new Date()).toISOString().split('T')[0],
-        requestUrl = apiUrl + dateString;
+        apiUrl = 'http://api.ruoka.xyz/';
         
-    var req = new XMLHttpRequest();
+    updateMenus(selectedDate);
     
-    req.addEventListener('load', function() {
-        var context = JSON.parse(this.responseText);
+    document.getElementById('next-day').addEventListener('click', function(event) {
+        event.preventDefault();
         
-        contentElem.innerHTML = template(context);
+        selectedDate.add(1, 'day');
+        
+        updateMenus(selectedDate);
     });
     
-    req.open('get', requestUrl, true);
-    req.send();
+    document.getElementById('prev-day').addEventListener('click', function(event) {
+        event.preventDefault();
+        
+        selectedDate.subtract(1, 'day');
+        
+        updateMenus(selectedDate);
+    });
+        
+    
+    function updateMenus(date) {
+        var dateString = date.format('YYYY-MM-DD'),
+            requestUrl = apiUrl + dateString;
+            
+        var req = new XMLHttpRequest();
+        
+        req.addEventListener('load', function() {
+            var context = JSON.parse(this.responseText);
+            context.date = date.format('D.M.');
+            
+            contentElem.innerHTML = template(context);
+        });
+        
+        req.open('get', requestUrl, true);
+        req.send();
+    }
 })();
